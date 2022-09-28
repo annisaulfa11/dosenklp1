4f39a233c897a5d8dc2a5082000ed35ee2983ded
muhammadaafiff <90713542+muhammadaafiff@users.noreply.github.com>
2022-09-29 01:44:41 +0700

    layout pertama.

---
+++ app/src/main/java/com/example/dosenklp1/PopupPembatalanTA.java
@@ -0,0 +1,13 @@
+package com.example.dosenklp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopupPembatalanTA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pembatalanta_popup);
+    }
+}
