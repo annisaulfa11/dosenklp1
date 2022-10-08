e269c3605e980694011b1d5952a8b4b2ca392204
annisaulfa11 <annisaulfasra@gmail.com>
2022-10-08 17:33:29 +0700

    logbook

--- app/src/main/java/com/example/dosenklp1/DetailLogbookActivity.java
+++ app/src/main/java/com/example/dosenklp1/DetailLogbookActivity.java
@@ -2,7 +2,9 @@ package com.example.dosenklp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DetailLogbookActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class DetailLogbookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_logbook);
+    }
+
+    public void main(View view) {
+        Intent main = new Intent(this, ListLogbookActivity.class);
+        startActivity(main);
     }
 }
