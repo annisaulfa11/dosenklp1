81671781381d6f9742f9a6e659f150a0e703c7cf
muhammadaafiff <90713542+muhammadaafiff@users.noreply.github.com>
2023-01-05 19:43:55 +0700

    uas detailLogbookActivity

--- app/src/main/java/com/example/dosenklp1/DetailLogbookActivity.java
+++ app/src/main/java/com/example/dosenklp1/DetailLogbookActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class DetailLogbookActivity extends AppCompatActivity {
+public class DetailLogbookActivity<intentdetaillb> extends AppCompatActivity {
+
+    TextView textViewTanggalLogbook, editTextKegiatanLogbook;
+    String tanggallb, progresslb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +18,20 @@ public class DetailLogbookActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_logbook);
     }
 
-    public void main(View view) {
-        Intent main = new Intent(this, ListLogbookActivity.class);
-        startActivity(main);
+    Intent intentdetaillb = getIntent();
+        if(intentdetaillb != null){
+        tanggallb = intentdetaillb.getStringExtra("tanggallb");
+        progresslb = intentdetaillb.getStringExtra("progreslb");
+
+        textViewTanggalLogbook = findViewById(R.id.textViewTanggalLogbook);
+        textViewTanggalLogbook.setText((tanggallb));
+        editTextKegiatanLogbook = findViewById(R.id.editTextKegiatanLogbook);
+        editTextKegiatanLogbook.setText(progresslb);
+
+
+
+        public void main(View view) {
+            Intent main = new Intent(this, ListLogbookActivity.class);
+            startActivity(main);
+        }
     }
-}
