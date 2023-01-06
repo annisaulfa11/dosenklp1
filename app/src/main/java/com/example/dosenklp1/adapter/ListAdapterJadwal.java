208990459c5d68059b038fdf1ce83792d070780a
RantiAgustin1911522028 <rantiagustin070@gmail.com>
2023-01-06 16:43:30 +0700

    recyclerview

--- app/src/main/java/com/example/dosenklp1/adapter/ListAdapterJadwal.java
+++ app/src/main/java/com/example/dosenklp1/adapter/ListAdapterJadwal.java
@@ -3,46 +3,118 @@ package com.example.dosenklp1.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dosenklp1.R;
+import com.example.dosenklp1.fragments.JadwalFragment;
+import com.example.dosenklp1.models.Bimbingan;
 import com.example.dosenklp1.models.Data;
+import com.example.dosenklp1.models.Jadwal;
+import com.example.dosenklp1.models.Logbook;
+
+import java.util.ArrayList;
 
 
 public class ListAdapterJadwal extends  RecyclerView.Adapter<ListAdapterJadwal.MyViewHolder>{
 
-    String[] list;
+    ArrayList<Jadwal> ListJadwal = new ArrayList<>();
+    ItemJadwalClickListener jadwalClickListener;
 
-    public ListAdapterJadwal(String[] list){
-        this.list = Data.names;
+    public ListAdapterJadwal(ArrayList<Jadwal> ListJadwal){
+        this.ListJadwal = ListJadwal;
     }
+
+    public ListAdapterJadwal (ArrayList<Jadwal> ListJadwal, ListAdapterJadwal.ItemJadwalClickListener jadwalClickListener) {
+        this.ListJadwal = ListJadwal;
+        this.jadwalClickListener = jadwalClickListener;
+    }
+
+    public void setListJadwal(ArrayList<Jadwal> ListJadwal) {
+        this.ListJadwal = ListJadwal;
+    }
+
+    public void setJadwalClickListener(ListAdapterJadwal.ItemJadwalClickListener jadwalClickListener) {
+        this.jadwalClickListener = jadwalClickListener;
+    }
+
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_jadwal, parent, false));
+    //membuat viewholder untuk menampilkan data
+    public ListAdapterJadwal.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ListAdapterJadwal.MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_jadwal, parent, false));
     }
 
     @Override
+    //untuk menempatkan data ke recyclerview
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.textView.setText(Data.names[position]);
+        Jadwal jadwal = ListJadwal.get(position);
+        holder.ImageProfil.setImageResource(R.drawable.ic_launcher_background);
+        holder.textNama.setText(jadwal.getNama());
+        holder.textNIM.setText(jadwal.getNim());
+        holder.textHariSeminar.setText(jadwal.getHariSeminar());
+        holder.textRuangSeminar.setText(jadwal.getRuangSeminar());
+        holder.textTimeSeminar.setText(jadwal.getWaktuSeminar());
+        holder.textHariSidang.setText(jadwal.getHariSidang());
+        holder.textRuangSidang.setText(jadwal.getRuangSidang());
+        holder.textWaktuSidang.setText(jadwal.getWaktuSidang());
+
     }
 
     @Override
     public int getItemCount() {
-        return Data.names.length;
+        return ListJadwal.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public interface ItemJadwalClickListener {
+        void onItemJadwalClick(Jadwal jadwal);
+    }
 
-        TextView textView;
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ImageProfil;
+        TextView textNama, textNIM, textHariSeminar, textRuangSeminar, textTimeSeminar, textHariSidang, textRuangSidang, textWaktuSidang;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            textView = itemView.findViewById(R.id.textViewNamaMahasiswa);
+            ImageProfil = itemView.findViewById(R.id.circleImageViewProfilePicture);
+            textNama = itemView.findViewById(R.id.textViewNamaMahasiswa);
+            textNIM = itemView.findViewById(R.id.textViewNIM);
+            textHariSeminar = itemView.findViewById(R.id.hariSeminar);
+            textRuangSeminar = itemView.findViewById(R.id.ruangSeminar);
+            textTimeSeminar = itemView.findViewById(R.id.timeSeminar);
+            textHariSidang = itemView.findViewById(R.id.hariSidang);
+            textRuangSidang = itemView.findViewById(R.id.ruangSidang);
+            textWaktuSidang = itemView.findViewById(R.id.timeSidang);
+
+            textHariSidang.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Jadwal jadwal = ListJadwal.get(getBindingAdapterPosition());
+                    jadwalClickListener.onItemJadwalClick(jadwal);
+                }
+            });
+
+            textRuangSidang.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Jadwal jadwal = ListJadwal.get(getBindingAdapterPosition());
+                    jadwalClickListener.onItemJadwalClick(jadwal);
+                }
+            });
+
+            textWaktuSidang.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Jadwal jadwal = ListJadwal.get(getBindingAdapterPosition());
+                    jadwalClickListener.onItemJadwalClick(jadwal);
+                }
+            });
+
         }
     }
 }
--- app/src/main/java/com/example/dosenklp1/fragments/JadwalFragment.java
+++ app/src/main/java/com/example/dosenklp1/fragments/JadwalFragment.java
@@ -1,21 +1,45 @@
 package com.example.dosenklp1.fragments;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dosenklp1.MainActivity;
 import com.example.dosenklp1.R;
 import com.example.dosenklp1.adapter.ListAdapterJadwal;
-import com.example.dosenklp1.models.Data;
+import com.example.dosenklp1.models.Bimbingan;
+import com.example.dosenklp1.models.Jadwal;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class JadwalFragment extends Fragment {
+public class JadwalFragment extends Fragment implements ListAdapterJadwal.ItemJadwalClickListener{
+
+    private NotificationManagerCompat notificationManager;
+    private static final String CHANNEL_ID;
+
+    static {
+        CHANNEL_ID = "test";
+    }
 
 
     @Override
@@ -25,8 +49,98 @@ public class JadwalFragment extends Fragment {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_jadwal, container, false);
         RecyclerView recyclerView = view.findViewById(R.id.jadwalData);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
-        recyclerView.setAdapter(new ListAdapterJadwal(Data.names));
+        ListAdapterJadwal adapterJadwal = new ListAdapterJadwal(getJadwal());
+        recyclerView.setAdapter(adapterJadwal);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("notifikasi", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        Log.d("pesan", token);
+                    }
+                });
+
+        notificationManager = NotificationManagerCompat.from(this.getContext());
+        createNotificationChannel();
 
         return view;
+    }
+
+    private ArrayList<Jadwal> getJadwal() {
+        ArrayList<Jadwal> ListJadwal = new ArrayList<>();
+        ListJadwal.add(new Jadwal(
+                null,
+                "Ranti Agustin",
+                "1911522028",
+                "Senin/2-01-2023",
+                "Ruang Seminar JSI",
+                "10.00 WIB",
+                "Senin/8-01-2023",
+                "Ruang Sidang SI",
+                "10.00 WIB"
+        ));
+
+        ListJadwal.add(new Jadwal(
+                null,
+                "Annisa Ulfa",
+                "2011522015",
+                "Selasa/3-01-2023",
+                "Ruang Seminar JSI",
+                "10.00 WIB",
+                "Selasa/9-01-2023",
+                "Ruang Sidang SI",
+                "10.00 WIB"
+        ));
+        ListJadwal.add(new Jadwal(
+                null,
+                "Muhammad Afif",
+                "2011522030",
+                "Rabu/4-01-2023",
+                "Ruang Seminar JSI",
+                "10.00 WIB",
+                "Rabu/10-01-2023",
+                "Ruang Sidang SI",
+                "10.00 WIB"
+        ));
+        return ListJadwal;
+    }
+
+
+    @Override
+    public void onItemJadwalClick(Jadwal jadwal) {
+        Intent resultIntent = new Intent(this.getContext(), MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this.getContext());
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0,
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this.getContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_notifications_none_24)
+                .setContentTitle("Jadwal masuk")
+                .setContentText("Contoh Notifikasi")
+                .setContentIntent(resultPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        notificationManager.notify(101, builder.build());
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Jadwal masuk";
+            String description = "Channel untuk notifikasi jadwal";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
